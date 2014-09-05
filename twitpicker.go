8106fbf3d45a39df0f80cad53723c20694436023
Atsushi Takayama <taka.atsushi@gmail.com>
2014-09-06 08:17:55 +0900

    adding user agent works

--- twitpicker.go
+++ twitpicker.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 )
 
 func main() {
-	resp, err := client.Get("http://twitpic.com/photos/harukasan.json")
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", "http://twitpic.com/photos/harukasan.json", nil)
+	req.Header.Add("User-Agent", "Mozilla/5.0")
+
+	resp, err := client.Do(req)
 
 	if err != nil {
 		fmt.Printf("error: %s\n", err.Error())
